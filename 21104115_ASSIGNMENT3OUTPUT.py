8d8051fb1b2b59a5a06cad736527b43477ec0af9
Harsimrat-KAURGILL <97609661+Harsimrat-KAURGILL@users.noreply.github.com>
2022-02-15 14:52:29 +0530

    Add files via upload

---
+++ 21104115_ASSIGNMENT3OUTPUT.py
@@ -0,0 +1,48 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\ACER\AppData\Local\Programs\Python\Python310\21104115_Harsimrat_Pythonassignment3.py
+Enter a string:Harsimratkaur
+Enter the character to check its frequency:r
+r occurs 3 times
+Enter day:24
+Enter month :9
+Enter year:2003
+Next Date is: 25 / 9 / 2003
+[(2, 4), (5, 25), (9, 81)]
+Enter your points:9
+Your Grade is: A and you performance is Excellent
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+Enter the name:Simrat
+Enter SID:115
+Do you want to add more elements in the list?(Enter "Y" for yes and "N" for no)Y
+Enter the name:Sehaj
+Enter SID:12
+Do you want to add more elements in the list?(Enter "Y" for yes and "N" for no)Y
+Enter the name:Jaap
+Enter SID:109
+Do you want to add more elements in the list?(Enter "Y" for yes and "N" for no)N
+Student details= {'115': 'Simrat', '12': 'Sehaj', '109': 'Jaap'}
+[('109', 'Jaap'), ('12', 'Sehaj'), ('115', 'Simrat')]
+Sorted dictionary using SID:
+('109', 'Jaap') ('115', 'Simrat') ('12', 'Sehaj') 
+Enter SID from dictionary to search for the corresponding names:115
+Print name of the student using SID: ('115', 'Simrat')
+Enter number of terms:6
+0
+1
+1
+2
+3
+5
+average= 2.0
+{1, 3, 5, 6, 8}
+{3, 6, 8, 9, 13, 17}
+{1, 2, 4, 5}
+{6, 7, 8, 9, 10}
+{10, 7}
